efc8475819970b2d285ac48939de933234ad2e3f
Arthur Silva Mendonça <arthu91@gmail.com>
2023-01-03 13:33:47 -0300

    realizando entrega (nao esta completa)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,35 @@
 /* Monte sua lógica aqui */
+
+const ul = document.querySelector(".main__list")
+
+async function createCard (){
+
+    const listaDePokemons = await getPokemons()
+
+    listaDePokemons.results.forEach(pokemon => {
+        const numeroNaPokedex = pokemon.url.slice(34,-1)
+
+        ul.insertAdjacentHTML("beforeend" ,`
+        <li class = "card__container"> 
+            <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name} >
+        
+        <h3>${pokemon.name}</h3>
+        </li>
+        `
+        )
+    })
+}
+createCard()
+
+
+async function teste(){
+    const listaDePokemons = await getPokemons()
+
+    let resultado = listaDePokemons.results[0].name
+
+    return console.log(resultado)
+
+}
+
+teste()
+
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,65 @@
 /* Monte a lógica das requisições aqui */
+
+async function getPokemons(){
+
+    const loading = document.querySelector(".loading")
+    const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=500&offset=20`)
+
+    .then (res => res.json()) 
+    .catch(error => console.log(error))
+
+    loading.classList.add("hidden")
+    return pokemons
+}
+
+// --------------------------------------------------------
+
+async function getPokemonByName(name){
+    
+    const listaDePokemons = await getPokemons()
+    
+    
+    console.log(listaDePokemons.results)
+    
+    
+    
+    listaDePokemons.results.forEach(pokemon => { 
+        const numeroNaPokedex = pokemon.url.slice(34,-1)
+        // let pokemonID = listaDePokemons.results[numeroNaPokedex].name
+
+        
+        console.log(pokemon.name)
+        if(  name == listaDePokemons.results[numeroNaPokedex].name)
+        {
+            ul.innerHTML = ""
+
+            ul.insertAdjacentHTML("beforeend" ,`
+            <li class = "card__container"> 
+                <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name} >
+            
+            <h3>${pokemon.name}</h3>
+            </li>
+            `
+            )
+        }
+    })
+
+    
+}
+
+// ------------------------------------------------------------------ USAR O ADJACENT HTML NA FUNÇÃO ABAIXO; USAR A FUNÇÃO ACIMA APENAS PRA CAPTURAR O PKMN PELO NOME
+
+function renderPkmnByName(){
+    const searchInput = document.querySelector("input").value
+    const button = document.querySelector("button")
+
+    button.addEventListener("click", (element) => {
+        
+        getPokemonByName(searchInput.toLocaleLowerCase().trim())
+    })
+
+}
+
+renderPkmnByName()
+
+console.log(document.querySelector("input"))
